4ed7d5f979c664402ca362c05a85047d5d807014
Сабуров Александр Владимирович <a.saburov@softmg.ru>
2024-12-07 11:57:32 +0300

    feat: set maximum date for date input to today

--- src/components/UiInputDate.vue
+++ src/components/UiInputDate.vue
@@ -9,6 +9,8 @@ interface Props {
 const props = withDefaults(defineProps<Props>(), {
 })
 
+const maxDate = new Date().toISOString().split("T")[0];
+
 const input = ref<HTMLInputElement | null>(null)
 
 const emit = defineEmits(['update:modelValue'])
@@ -19,8 +21,6 @@ watch(
     () => [props.modelValue, input.value] as const,
     ([newValue, input]) => {
         if (isElement(input)) {
-            input.max = new Date().toISOString().split("T")[0];
-
             input.valueAsDate = newValue
         }
     },
@@ -56,7 +56,7 @@ const updateValue = (event: Event) => {
             <slot name="label" />
         </label>
 
-        <input ref="input" v-on:change="updateValue" type="date" :placeholder="props.placeholder"
+        <input ref="input" :max="maxDate" v-on:change="updateValue" type="date" :placeholder="props.placeholder"
             class="mt-2 block w-full placeholder-gray-400/70 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 " />
 
         <!-- <p class="mt-3 text-xs text-gray-400 ">Lorem ipsum dolor sit amet consectetur adipisicing
